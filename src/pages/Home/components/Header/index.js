f3222f3fb287ef95a635117ac12e6b8e96df1977
Marlus <marlus_fox@hotmail.com>
2023-11-16 09:50:08 -0300

    Atualização

--- src/pages/Home/components/Header/index.js
+++ src/pages/Home/components/Header/index.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
-export default function Header(){
+const StatusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 10 : 42;
+
+export default function Header({name}){
     return(
         <View style={styles.container}>
-            <Text style={styles.username}>
-                Marlus (cliente)
-            </Text>
+            <Text style={styles.username}>{name}</Text>
             <TouchableOpacity style={styles.buttomuser} activeOpacity={0.8}>
-            <FontAwesome name="user-circle" size={35} color="white" />
+                <FontAwesome name="user-circle" size={35} color="white" />
             </TouchableOpacity>
         </View>
     );
@@ -17,5 +17,24 @@ export default function Header(){
 const styles = StyleSheet.create({
     container:{
         backgroundColor:'#121212',
-    }
+        paddingTop: StatusBarHeight,
+        flexDirection: 'row',
+        paddingStart: 15,
+        paddingEnd: 15,
+        paddingBottom: 30,
+        justifyContent:'space-between',
+
+    },
+
+    username:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#ffff',
+    },
+
+    buttomuser:{
+        width: 35,
+        height: 35,
+    },
+
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,20 @@
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 
-const StatusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 10 : 42;
 
 export default function Home() {
   return (
-    Header;
+    <View syle={StyleSheet.container}>
+      <Header name="Marlus (Cliente)" />
+      <Text>Pagina home!</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff'
 
+  }
 });
