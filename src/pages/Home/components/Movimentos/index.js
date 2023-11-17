c3a0033fbd7a0ffc0da44e96d8797de0cd92a348
Marlus <marlus_fox@hotmail.com>
2023-11-17 09:45:40 -0300

    Att

---
+++ src/pages/Home/components/Movimentos/index.js
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+
+export default function Movimentos({data}) {
+    const [showValue, setShowValue]=useState(false);
+ return (
+   <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
+    <Text style={styles.date}>{data.date}</Text>
+        <View style={styles.content}>
+            <Text style={styles.label}>{data.label}</Text>
+            {showValue ? (
+                <Text style={data.type === 1 ? styles.value : styles.despesas}>
+                    {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
+                </Text>
+            ) : (
+                <View style={styles.hide}>
+                    
+                </View>
+            )}
+        </View>
+
+    </TouchableOpacity>
+  );
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex: 1,
+        marginBottom: 15,
+        borderBottomWidth: 0.5,
+        borderBlockColor: '#2e2e2e'
+
+},
+
+content:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    marginTop:2,
+    marginBottom: 4,
+},
+
+date:{
+    color: '#121212',
+    fontWeight:'bold',
+
+},
+
+label:{
+    fontWeight:'bold',
+    fontSize: 15,
+},
+
+value:{
+    fontSize: 15,
+    fontWeight: 'bold',
+    color:'#168a1c'
+},
+
+despesas:{
+    fontSize: 15,
+    color: '#bf1206',
+    fontWeight:'bold',
+},
+
+hide:{
+    backgroundColor: '#DADADA',
+    width: 80,
+    borderRadius: 8,
+    height: 15,
+    marginTop: 8,
+}
+})
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header';
 import Balance from './components/Balance';
+import Movimentos from './components/Movimentos';
 
+
+
+const lista = [{
+  id: 1,
+  label: 'Boleto de luz',
+  value: '150,57',
+  date: '17/08/2023',
+  type: 0 //despesa
+},
+
+{
+  id: 2,
+  label: 'Pix X',
+  value: '200,57',
+  date: '12/08/2023',
+  type: 1 //entradas
+},
+
+{
+  id: 3,
+  label: 'Salário',
+  value: '1.500,57',
+  date: '3/08/2023',
+  type: 1 //entradas
+},
+
+]
 
 export default function Home() {
   return (
     <View syle={StyleSheet.container}>
       <Header name="Marlus (Cliente)" />
-      <Balance saldo="9.250,00" gastos="-527,50"/>
+      <Balance saldo="9.111,00" gastos="-527,50"/>
 
+      <Text style={styles.titulo}>Últimas movimentações</Text>
+      <FlatList style={styles.list} data={lista} keyExtractor={(item) => String(item.id)} showsVerticalScroLLIndicator={false} renderItem={({item}) => <Movimentos data={item}/>}/>
+    
     </View>
   );
 }
@@ -16,7 +47,20 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff'
+    backgroundColor: '#2e2e2e',
+  },
 
+  titulo:{
+    fontSize: 14,
+    fontWeight:'bold',
+    marginStart: 15,
+    marginEnd: 15,
+    marginTop: 15,
+  },
+
+  list:{
+    marginStart: 15,
+    
   }
+
 });
